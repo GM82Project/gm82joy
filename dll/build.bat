3ffdb26f37e4fc1c539dc59310bbb363d00fc462
omicronrex <65692652+omicronrex@users.noreply.github.com>
2021-05-24 18:27:00 -0300

    add optimizations to build script
    also some other stuff

--- dll/build.bat
+++ dll/build.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat"
 
-cl gm82joy.c /link /dll /out:gm82joy.dll
+cl gm82joy.c /O2 /GL /link /dll /out:gm82joy.dll
 
 pause
